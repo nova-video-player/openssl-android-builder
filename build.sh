c8abdf87d9d0c0f6decfa35744e071ddf26ccd31
Courville Software <software@courville.org>
2025-10-05 19:56:40 +0200

    3.5.4 upstep

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ LOCAL_PATH=$($READLINK -f .)
 
 if [ ! -d openssl.git ]; then
 #  git clone https://github.com/openssl/openssl openssl.git --bare --depth=1 -b OpenSSL_1_1_1w
-  git clone https://github.com/openssl/openssl openssl.git --depth=1 -b openssl-3.5.2
+  git clone https://github.com/openssl/openssl openssl.git --depth=1 -b openssl-3.5.4
 fi
 
 OPENSSL_BARE_PATH=$($READLINK -f openssl.git)
